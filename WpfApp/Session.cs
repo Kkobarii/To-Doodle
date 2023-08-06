11b9804edfcb3b365203f546abf08bdbc55e92c1
Barbora Kovalská <barca.kovalska@gmail.com>
2023-08-06 14:36:34 +0200

    Added Login window

---
+++ WpfApp/Session.cs
@@ -0,0 +1,14 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Session
+    {
+        public static User? User { get; set; }
+    }
+}
